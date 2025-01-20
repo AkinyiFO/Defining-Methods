f7dee6294a1a95abc76e9d965af4a7f933d753e3
AkinyiFO <oafridah@gmail.com>
2025-01-20 17:02:09 +0300

    Fin

--- src/main/java/dev/labs/s3/DefiningMethods.java
+++ src/main/java/dev/labs/s3/DefiningMethods.java
@@ -2,7 +2,12 @@ package dev.labs.s3;
 
 public class DefiningMethods {
   // Returns sum of values
+    int returnsSum (){
+        return 2+2;
+    }
 
   // Returns nothing - main method
-    
+  public void setName(){
+        
+  }
 }
